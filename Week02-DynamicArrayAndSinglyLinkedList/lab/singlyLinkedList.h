f54c6cf8704ca2c5e492a4d18a5c7666afdc7eb4
Petar Kolev <petar.kolev@ocado.com>
2023-10-07 16:23:52 +0300

    add work from today

--- Week02-DynamicArrayAndSinglyLinkedList/lab/singlyLinkedList.h
+++ Week02-DynamicArrayAndSinglyLinkedList/lab/singlyLinkedList.h
@@ -3,25 +3,139 @@
 template <class T>
 class SinglyLinkedList
 {
+private:
+    struct Box
+    {
+        T data;
+        Box* next;
+
+        Box(const T& data, Box* next = nullptr): data(data), next(next) {}
+    };
+
+    Box* head;
+    Box* tail;
+
+    void deallocate()
+    {
+        Box* temp = head;
+        while (temp != nullptr)
+        {
+            Box* toDelete = temp;
+            temp = temp->next;
+            delete toDelete;
+        }
+
+        this->head = this->tail = nullptr;
+    }
+
+    void copy(const SinglyLinkedList<T>& other)
+    {
+        if (other.head == nullptr)
+        {
+            this->head = this->tail = nullptr;
+            return;
+        }
+
+        this->head = new Box(other.head->data);
+        Box* temp = this->head;
+        Box* tempOther = other.head;
+        while (tempOther->next != other.tail)
+        {
+            temp->next = new Box(tempOther->next->data);
+            temp = temp->next;
+            tempOther = tempOther->next;
+        }
+        this->tail = new Box(other.tail->data);
+        temp->next = this->tail;
+    }
+
 public:
-    SinglyLinkedList<T>();
-    SinglyLinkedList<T>& operator = (const SinglyLinkedList<T>& other);
-    SinglyLinkedList<T>(const SinglyLinkedList<T>& other);
-    ~SinglyLinkedList<T>();
+    SinglyLinkedList<T>()
+      : head(nullptr), tail(nullptr) {}
 
-    SinglyLinkedList(std::size_t count, const T& value = T());
+    SinglyLinkedList<T>& operator = (const SinglyLinkedList<T>& other)
+    {
+        if (this != &other)
+        {
+            this->deallocate();
+            this->copy(other);
+        }
 
-    std::size_t size() const;
+        return *this;
+    }
 
-    T& front();
-    const T& front() const;
-    T& back();
-    const T& back() const;
+    SinglyLinkedList<T>(const SinglyLinkedList<T>& other)
+    {
+        this->copy(other);
+    }
 
-    void clear();
+    ~SinglyLinkedList<T>() 
+    {
+        this->deallocate();
+    };
+
+    SinglyLinkedList(std::size_t count, const T& value = T())
+    {
+        for (std::size_t i = 0; i < count; ++i)
+        {
+            this->push_back(value);
+        }
+    }
+
+    std::size_t size() const
+    {
+        if (head == nullptr)
+        {
+            return 0;
+        }
+        std::size_t result = 1;
+
+        Box* temp = head;
+        while (temp->next != nullptr)
+        {
+            temp = temp->next;
+            ++result;
+        }
+
+        return result;
+    }
+
+    T& front()
+    {
+        return this->head->data;
+    }
+    const T& front() const
+    {
+        return this->head->data;
+    }
+    T& back()
+    {
+        return this->tail->data;
+    }
+    const T& back() const
+    {
+        return this->tail->data;
+    }
+
+    void clear()
+    {
+        this->deallocate();
+    }
     bool empty() const;
 
-    void push_back(const T& element);
+    void push_back(const T& element)
+    {
+        if (this->head == nullptr)
+        {
+            this->head = new Box(element);
+            this->tail = this->head;
+        }
+        else
+        {
+            this->tail->next = new Box(element);
+            this->tail = this->tail->next;
+        }
+    }
     void pop_back();
 
     void push_front(const T& element);
--- Week02-DynamicArrayAndSinglyLinkedList/lab/singlyLinkedListTests.cpp
+++ Week02-DynamicArrayAndSinglyLinkedList/lab/singlyLinkedListTests.cpp
@@ -61,251 +61,251 @@ TEST_CASE("List create/destroy")
     }
 }
 
-TEST_CASE("List back/front modify") 
-{
-	SECTION("Push_back and pop_back all") 
-    {
-		SinglyLinkedList<int> instance;
-        for (int c = 0; c < 21; c++) 
-        {
-            instance.push_back(0);
-        }
-        REQUIRE(instance.size() == 21);
+// TEST_CASE("List back/front modify") 
+// {
+// 	SECTION("Push_back and pop_back all") 
+//     {
+// 		SinglyLinkedList<int> instance;
+//         for (int c = 0; c < 21; c++) 
+//         {
+//             instance.push_back(0);
+//         }
+//         REQUIRE(instance.size() == 21);
 
-        for (int c = 0; c < 21; c++) 
-        {
-            instance.pop_back();
-        }
-        REQUIRE(instance.size() == 0);
-	}
+//         for (int c = 0; c < 21; c++) 
+//         {
+//             instance.pop_back();
+//         }
+//         REQUIRE(instance.size() == 0);
+// 	}
 
-	SECTION("Repeated push_back/pop_back") 
-    {
-		SinglyLinkedList<int> instance;
-        for (int c = 0; c < 100; c++) 
-        {
-            instance.push_back(0);
-            instance.pop_back();
-        }
-        REQUIRE(instance.size() == 0);
-	}
+// 	SECTION("Repeated push_back/pop_back") 
+//     {
+// 		SinglyLinkedList<int> instance;
+//         for (int c = 0; c < 100; c++) 
+//         {
+//             instance.push_back(0);
+//             instance.pop_back();
+//         }
+//         REQUIRE(instance.size() == 0);
+// 	}
 
-	SECTION("Repeated push_back/back") 
-    {
-		SinglyLinkedList<int> instance;
-        int value = 0;
-        for (int c = 0; c < 100; c++) 
-        {
-            instance.push_back(value);
-            REQUIRE(instance.back() == value);
-            ++value;
-        }
-        REQUIRE(instance.size() == 100);
-	}
+// 	SECTION("Repeated push_back/back") 
+//     {
+// 		SinglyLinkedList<int> instance;
+//         int value = 0;
+//         for (int c = 0; c < 100; c++) 
+//         {
+//             instance.push_back(value);
+//             REQUIRE(instance.back() == value);
+//             ++value;
+//         }
+//         REQUIRE(instance.size() == 100);
+// 	}
 
-	SECTION("Push_front and pop_front all") 
-    {
-		SinglyLinkedList<int> instance;
-        for (int c = 0; c < 21; c++) 
-        {
-            instance.push_front(0);
-        }
-        REQUIRE(instance.size() == 21);
+// 	SECTION("Push_front and pop_front all") 
+//     {
+// 		SinglyLinkedList<int> instance;
+//         for (int c = 0; c < 21; c++) 
+//         {
+//             instance.push_front(0);
+//         }
+//         REQUIRE(instance.size() == 21);
 
-        for (int c = 0; c < 21; c++) 
-        {
-            instance.pop_front();
-        }
-        REQUIRE(instance.size() == 0);
-	}
+//         for (int c = 0; c < 21; c++) 
+//         {
+//             instance.pop_front();
+//         }
+//         REQUIRE(instance.size() == 0);
+// 	}
 
-	SECTION("Repeated push_front/pop_front") 
-    {
-		SinglyLinkedList<int> instance;
-        for (int c = 0; c < 100; c++) 
-        {
-            instance.push_front(0);
-            instance.pop_front();
-        }
-        REQUIRE(instance.size() == 0);
-	}
+// 	SECTION("Repeated push_front/pop_front") 
+//     {
+// 		SinglyLinkedList<int> instance;
+//         for (int c = 0; c < 100; c++) 
+//         {
+//             instance.push_front(0);
+//             instance.pop_front();
+//         }
+//         REQUIRE(instance.size() == 0);
+// 	}
 
-	SECTION("Repeated push_front/front") 
-    {
-		SinglyLinkedList<int> instance;
-        int value = 0;
-        for (int c = 0; c < 100; c++) 
-        {
-            instance.push_front(value);
-            REQUIRE(instance.front() == value);
-            ++value;
-        }
-        REQUIRE(instance.size() == 100);
-	}
-}
+// 	SECTION("Repeated push_front/front") 
+//     {
+// 		SinglyLinkedList<int> instance;
+//         int value = 0;
+//         for (int c = 0; c < 100; c++) 
+//         {
+//             instance.push_front(value);
+//             REQUIRE(instance.front() == value);
+//             ++value;
+//         }
+//         REQUIRE(instance.size() == 100);
+// 	}
+// }
 
-TEST_CASE("List iterator access") 
-{
-	SECTION("Iterator creation and operators") 
-    {
-		SinglyLinkedList<int> instance(3, 0);
-        typename SinglyLinkedList<int>::Iterator it = instance.begin();
-        REQUIRE(*it == 0);
-        REQUIRE(it == instance.begin());
-        typename SinglyLinkedList<int>::Iterator end = instance.end();
-        REQUIRE(end == instance.end());
-        REQUIRE(++(--end) == instance.end());
-	}
+// TEST_CASE("List iterator access") 
+// {
+// 	SECTION("Iterator creation and operators") 
+//     {
+// 		SinglyLinkedList<int> instance(3, 0);
+//         typename SinglyLinkedList<int>::Iterator it = instance.begin();
+//         REQUIRE(*it == 0);
+//         REQUIRE(it == instance.begin());
+//         typename SinglyLinkedList<int>::Iterator end = instance.end();
+//         REQUIRE(end == instance.end());
+//         REQUIRE(++(--end) == instance.end());
+// 	}
 
-	SECTION("Iterator value modification") 
-    {
-		SinglyLinkedList<int> instance(3);
-        int value = 0;
+// 	SECTION("Iterator value modification") 
+//     {
+// 		SinglyLinkedList<int> instance(3);
+//         int value = 0;
         
-        for (typename SinglyLinkedList<int>::Iterator it = instance.begin(); it != instance.end(); ++it) 
-        {
-            *it = ++value;
-        }
+//         for (typename SinglyLinkedList<int>::Iterator it = instance.begin(); it != instance.end(); ++it) 
+//         {
+//             *it = ++value;
+//         }
 
-        value = 0;
-        for (typename SinglyLinkedList<int>::Iterator it = instance.begin(); it != instance.end(); ++it) 
-        {
-            REQUIRE(*it == ++value);
-        }
-	}
+//         value = 0;
+//         for (typename SinglyLinkedList<int>::Iterator it = instance.begin(); it != instance.end(); ++it) 
+//         {
+//             REQUIRE(*it == ++value);
+//         }
+// 	}
 	
-	SECTION("Iterator insert/erase") 
-    {
-		SinglyLinkedList<int> instance(1, 0);
-        int value = 0;
+// 	SECTION("Iterator insert/erase") 
+//     {
+// 		SinglyLinkedList<int> instance(1, 0);
+//         int value = 0;
 
-        instance.insert(instance.begin(), ++value);
-        REQUIRE(instance.front() == value);
+//         instance.insert(instance.begin(), ++value);
+//         REQUIRE(instance.front() == value);
 
-        instance.insert(instance.end(), ++value);
-        REQUIRE(instance.back() == value);
+//         instance.insert(instance.end(), ++value);
+//         REQUIRE(instance.back() == value);
 
-        SinglyLinkedList<int> copy = instance;
+//         SinglyLinkedList<int> copy = instance;
 
-        instance.erase(instance.begin());
-        REQUIRE(instance.front() == 0);
+//         instance.erase(instance.begin());
+//         REQUIRE(instance.front() == 0);
 
-        instance.erase(--instance.end());
-        REQUIRE(instance.back() == 0);
-        REQUIRE(instance.size() == 1);
+//         instance.erase(--instance.end());
+//         REQUIRE(instance.back() == 0);
+//         REQUIRE(instance.size() == 1);
 
-        copy.erase(copy.begin(), copy.end());
-        REQUIRE(copy.empty());
-    }
-}
+//         copy.erase(copy.begin(), copy.end());
+//         REQUIRE(copy.empty());
+//     }
+// }
 
-TEST_CASE("List element operations") 
-{
-	SECTION("Splice after") 
-    {
-		SinglyLinkedList<int> source;
-        SinglyLinkedList<int> destination;
-        std::vector<int*> sourceItems;
-        int value = 0;
-        const int itemCount = 2;
+// TEST_CASE("List element operations") 
+// {
+// 	SECTION("Splice after") 
+//     {
+// 		SinglyLinkedList<int> source;
+//         SinglyLinkedList<int> destination;
+//         std::vector<int*> sourceItems;
+//         int value = 0;
+//         const int itemCount = 2;
         
-        for (int c = 0; c < itemCount; c++) 
-        {
-            destination.push_back(value);
-            ++value;
-        }
+//         for (int c = 0; c < itemCount; c++) 
+//         {
+//             destination.push_back(value);
+//             ++value;
+//         }
 
-        for (int c = 0; c < itemCount; c++) 
-        {
-            source.push_back(value);
-            sourceItems.push_back(&source.back());
-            ++value;
-        }
+//         for (int c = 0; c < itemCount; c++) 
+//         {
+//             source.push_back(value);
+//             sourceItems.push_back(&source.back());
+//             ++value;
+//         }
 
-        typename SinglyLinkedList<int>::Iterator begin1 = destination.begin();
-        destination.splice_after(++begin1, source);
+//         typename SinglyLinkedList<int>::Iterator begin1 = destination.begin();
+//         destination.splice_after(++begin1, source);
 
-        REQUIRE(destination.size() == 4);
-        REQUIRE(source.size() == 0);
+//         REQUIRE(destination.size() == 4);
+//         REQUIRE(source.size() == 0);
 
-        typename SinglyLinkedList<int>::Iterator it = destination.begin();
-        REQUIRE(&*(++it) == sourceItems[0]);
-        REQUIRE(&*(++it) == sourceItems[1]);
-	}
+//         typename SinglyLinkedList<int>::Iterator it = destination.begin();
+//         REQUIRE(&*(++it) == sourceItems[0]);
+//         REQUIRE(&*(++it) == sourceItems[1]);
+// 	}
 
-	SECTION("Merge") 
-    {
-		SinglyLinkedList<int> destination;
-        SinglyLinkedList<int> source;
-        std::vector<int*> sourceItems;
-        int value = 0;
-        const int itemCount = 10;
+// 	SECTION("Merge") 
+//     {
+// 		SinglyLinkedList<int> destination;
+//         SinglyLinkedList<int> source;
+//         std::vector<int*> sourceItems;
+//         int value = 0;
+//         const int itemCount = 10;
 
-        for (int c = 0; c < itemCount; c++) 
-        {
-            if (c % 2 == 0) 
-            {
-                destination.push_back(value);
-            }
-            else 
-            {
-                source.push_back(value);
-                sourceItems.push_back(&source.back());
-            }
-            ++value;
-        }
+//         for (int c = 0; c < itemCount; c++) 
+//         {
+//             if (c % 2 == 0) 
+//             {
+//                 destination.push_back(value);
+//             }
+//             else 
+//             {
+//                 source.push_back(value);
+//                 sourceItems.push_back(&source.back());
+//             }
+//             ++value;
+//         }
 
-        destination.merge(source);
+//         destination.merge(source);
 
-        REQUIRE(destination.size() == 10);
-        REQUIRE(source.size() == 0);
+//         REQUIRE(destination.size() == 10);
+//         REQUIRE(source.size() == 0);
 
-        typename SinglyLinkedList<int>::Iterator current = destination.begin();
-        typename SinglyLinkedList<int>::Iterator next = ++destination.begin();
-        for (; next != destination.end(); ++current, ++next) 
-        {
-            REQUIRE(*current <= *next);
-        }
+//         typename SinglyLinkedList<int>::Iterator current = destination.begin();
+//         typename SinglyLinkedList<int>::Iterator next = ++destination.begin();
+//         for (; next != destination.end(); ++current, ++next) 
+//         {
+//             REQUIRE(*current <= *next);
+//         }
 
-        current = destination.begin();
-        int sourceItemIx = 0;
-        for (int i = 0; current != destination.end(); ++i, ++current) {
-            if (i % 2 == 1) {
-                REQUIRE(&(*current) == sourceItems[sourceItemIx]);
-                sourceItemIx++;
-            }
-        }
-	}
+//         current = destination.begin();
+//         int sourceItemIx = 0;
+//         for (int i = 0; current != destination.end(); ++i, ++current) {
+//             if (i % 2 == 1) {
+//                 REQUIRE(&(*current) == sourceItems[sourceItemIx]);
+//                 sourceItemIx++;
+//             }
+//         }
+// 	}
 
-	SECTION("Unique") 
-    {
-		SinglyLinkedList<int> instance;
-        std::vector<int*> itemsLeft;
-        int value = 0;
-        const int itemCount = 10;
+// 	SECTION("Unique") 
+//     {
+// 		SinglyLinkedList<int> instance;
+//         std::vector<int*> itemsLeft;
+//         int value = 0;
+//         const int itemCount = 10;
 
-        for (int c = 0; c < itemCount; c++) 
-        {
-            instance.push_back(value);
+//         for (int c = 0; c < itemCount; c++) 
+//         {
+//             instance.push_back(value);
 
-            if (c % 2 == 0) 
-            {
-                itemsLeft.push_back(&instance.back());
-            }
-            else 
-            {
-                ++value;
-            }
-        }
+//             if (c % 2 == 0) 
+//             {
+//                 itemsLeft.push_back(&instance.back());
+//             }
+//             else 
+//             {
+//                 ++value;
+//             }
+//         }
 
-        instance.unique();
-        REQUIRE(instance.size() == 5);
+//         instance.unique();
+//         REQUIRE(instance.size() == 5);
 
-        int itemsLeftIx = 0;
-        for (typename SinglyLinkedList<int>::Iterator it = instance.begin(); it != instance.end(); ++it) 
-        {
-            REQUIRE(&(*it) == itemsLeft[itemsLeftIx]);
-            ++itemsLeftIx;
-        }
-	}
-}
+//         int itemsLeftIx = 0;
+//         for (typename SinglyLinkedList<int>::Iterator it = instance.begin(); it != instance.end(); ++it) 
+//         {
+//             REQUIRE(&(*it) == itemsLeft[itemsLeftIx]);
+//             ++itemsLeftIx;
+//         }
+// 	}
+// }
